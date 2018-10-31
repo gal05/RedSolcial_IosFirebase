c2987334d8867276c28b774c3c870baf229c4525
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2018-10-31 12:53:19 -0500

    outlet and action

--- RedSolcial_IosFirebase/IniciarSesionViewController.swift
+++ RedSolcial_IosFirebase/IniciarSesionViewController.swift
@@ -10,15 +10,21 @@ import UIKit
 
 class IniciarSesionViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func iniciarSesionTapped(_ sender: Any) {
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 
 
 }
