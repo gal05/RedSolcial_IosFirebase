83eb7ce858c0a8babd9503c999f1c0dff2a72e6d
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2018-11-07 17:28:36 -0500

    gg por ahi

---
+++ RedSolcial_IosFirebase/ElegirUsuarioViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ElegirUsuarioViewController.swift
+//  RedSolcial_IosFirebase
+//
+//  Created by Guerra Jose on 7/11/18.
+//  Copyright © 2018 Tecsup. All rights reserved.
+//
+
+import UIKit
+
+class ElegirUsuarioViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+
+}
