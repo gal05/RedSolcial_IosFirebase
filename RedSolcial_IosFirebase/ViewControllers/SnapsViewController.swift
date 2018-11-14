2c4dbdbc8498bde82bc22f9a32d21919cfdea167
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2018-11-14 15:33:54 -0500

    vamos bien aparentemente pag10

--- RedSolcial_IosFirebase/ViewControllers/SnapsViewController.swift
+++ RedSolcial_IosFirebase/ViewControllers/SnapsViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import Firebase
 
-class SnapsViewController: UIViewController {
+class SnapsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
 
+
+    
+    
+    @IBOutlet weak var tableView1: UITableView!
+    
+    var snpas :[Snap]=[]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView1.dataSource=self
+        tableView1.delegate=self
+    FIRDatabase.database().reference().child("usuarios").child(FIRAuth.auth()!.currentUser!.uid).child("snaps").observe(FIRDataEventType.childAdded, with: {(snapshot)in
+            let snap = Snap()
+            snap.imagenURL = (snapshot.value as! NSDictionary)["imagenURL"] as! String
+            snap.from = (snapshot.value as! NSDictionary)["from"] as! String
+            snap.descrip = (snapshot.value as! NSDictionary)["descripcion"] as! String
+            self.snpas.append(snap)
+            self.tableView1.reloadData()
+        })
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return snpas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let snap = snpas[indexPath.row]
+        cell.textLabel?.text=snap.from
+        return cell
+    }
+    
 
-
-    @IBAction func cerrarSesionTapped(_ sender: Any) {
+    /*@IBAction func cerrarSesionTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
-    }
+    }*/
     
 
 
