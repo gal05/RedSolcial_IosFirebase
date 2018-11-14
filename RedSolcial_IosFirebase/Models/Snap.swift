896e6906917207eee23fad1f20b170a3fec98665
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2018-11-14 14:39:31 -0500

    antes de la pagina 8 warnig!

---
+++ RedSolcial_IosFirebase/Models/Snap.swift
@@ -0,0 +1,14 @@
+//
+//  Snap.swift
+//  RedSolcial_IosFirebase
+//
+//  Created by Guerra Jose on 14/11/18.
+//  Copyright © 2018 Tecsup. All rights reserved.
+//
+
+import Foundation
+class Snap {
+    var imagenURL = ""
+    var descrip = ""
+    var  from = ""
+}
